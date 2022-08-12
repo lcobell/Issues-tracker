7752f0ec943ff24e42d1e38a51c7267e0001837e
lcobell <lcobell@gmail.com>
2022-08-12 10:17:28 -0500

    component pages created and react router is working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,26 @@
-import Dashboard from './components/Dashboard';
+import Dashboard from "./components/Dashboard";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import AllProjects from "./components/AllProjects";
+import Chat from "./components/Chat";
+import MyTeam from "./components/MyTeam";
+import ManageRole from "./components/ManageRole";
+import MyTickets from "./components/MyTickets";
 
 function App() {
-    return (
-        <div className="App">
-            <Dashboard />
-        </div>
-    );
+  return (
+    <div className="App">
+      <Dashboard />
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="AllProjects" element={<AllProjects />} />
+        <Route path="Chat" element={<Chat />} />
+        <Route path="MyTeam" element={<MyTeam />} />
+        <Route path="ManageRole" element={<ManageRole />} />
+        <Route path="MyTickets" element={<MyTickets />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,22 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { ThemeProvider } from '@mui/material/styles';
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-import theme from './theme';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { ThemeProvider } from "@mui/material/styles";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import theme from "./theme";
+import { BrowserRouter } from "react-router-dom";
+//Added BrowserRouter to the App component to make the app work with react-router-dom
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <React.StrictMode>
-        <ThemeProvider theme={theme}>
-            <App />
-        </ThemeProvider>
-    </React.StrictMode>
+  <React.StrictMode>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
