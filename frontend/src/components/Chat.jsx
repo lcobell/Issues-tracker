ddff679b1016f6183a5b20eff8cf9b2540cd685b
lcobell <lcobell@gmail.com>
2022-08-12 10:17:28 -0500

    component pages created and react router is working

--- frontend/src/components/Chat.jsx
+++ frontend/src/components/Chat.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React from 'react';
 
 const Chat = () => {
-  return <div className="App">Chat</div>
-}
+    return <div className="App">Chat</div>;
+};
 
-export default Chat
+export default Chat;
