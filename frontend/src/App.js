69c54cf61055232f5538359201400e3415474b90
Shannon Stephenson <stephenson.shannon29@gmail.com>
2022-08-13 14:17:38 -0400

    login and signup done need to make links usuable

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,32 @@
-import Dashboard from './components/Dashboard'
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import AllProjects from './components/projects/AllProjects'
-import Chat from './components/Chat'
-import MyTeam from './components/MyTeam'
-import ManageRole from './components/ManageRole'
-import MyTickets from './components/projects/MyTickets'
-import DashboardLayout from './components/DashboardLayout'
-import SignIn from './components/SignIn'
-import { CssBaseline } from '@mui/material'
+import Dashboard from './components/Dashboard';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import AllProjects from './components/projects/AllProjects';
+import Chat from './components/Chat';
+import MyTeam from './components/MyTeam';
+import ManageRole from './components/ManageRole';
+import MyTickets from './components/projects/MyTickets';
+import DashboardLayout from './components/DashboardLayout';
+import SignIn from './components/SignIn';
+import { CssBaseline } from '@mui/material';
 
 function App() {
-  return (
-    <div className="App">
-      <CssBaseline />
-      <DashboardLayout>
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="AllProjects" element={<AllProjects />} />
-          <Route path="Chat" element={<Chat />} />
-          <Route path="MyTeam" element={<MyTeam />} />
-          <Route path="ManageRole" element={<ManageRole />} />
-          <Route path="MyTickets" element={<MyTickets />} />
-          <Route path="Login" element={<SignIn />} />
-        </Routes>
-      </DashboardLayout>
-    </div>
-  )
+    return (
+        <div className="App">
+            <CssBaseline />
+            <DashboardLayout>
+                <Routes>
+                    <Route path="/" element={<Dashboard />} />
+                    <Route path="AllProjects" element={<AllProjects />} />
+                    <Route path="Chat" element={<Chat />} />
+                    <Route path="MyTeam" element={<MyTeam />} />
+                    <Route path="ManageRole" element={<ManageRole />} />
+                    <Route path="MyTickets" element={<MyTickets />} />
+                    <Route path="Login" element={<SignIn />} />
+                </Routes>
+            </DashboardLayout>
+        </div>
+    );
 }
 
-export default App
+export default App;
